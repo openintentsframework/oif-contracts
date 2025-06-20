86bf2bfcf21c9cd1993a893d278327cad234b0ea
Luiz <luiz.vasconcelos@openzeppelin.com>
2025-06-20 13:02:57 +0200

    solady ReentrancyGuard -> OZ ReentrancyGuard

--- src/integrations/CatsMulticallHandler.sol
+++ src/integrations/CatsMulticallHandler.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import { IERC20 } from "openzeppelin/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
-import { ReentrancyGuard } from "solady/utils/ReentrancyGuard.sol";
+import { ReentrancyGuard } from "openzeppelin/utils/ReentrancyGuard.sol";
 
 import { EfficiencyLib } from "the-compact/src/lib/EfficiencyLib.sol";
 
