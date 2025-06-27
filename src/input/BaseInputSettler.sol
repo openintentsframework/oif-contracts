13619843a96c211820e862539623bd4e5543412e
Alexander <git@lindgren.xyz>
2025-06-27 14:25:08 +0200

    remote dublicate SignatureCheckerLib import

--- src/input/BaseInputSettler.sol
+++ src/input/BaseInputSettler.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.26;
 import { EIP712 } from "solady/utils/EIP712.sol";
 import { SafeTransferLib } from "solady/utils/SafeTransferLib.sol";
 import { SignatureCheckerLib } from "solady/utils/SignatureCheckerLib.sol";
-import { SignatureCheckerLib } from "solady/utils/SignatureCheckerLib.sol";
 import { EfficiencyLib } from "the-compact/src/lib/EfficiencyLib.sol";
 
 import { IOIFCallback } from "../interfaces/IOIFCallback.sol";
