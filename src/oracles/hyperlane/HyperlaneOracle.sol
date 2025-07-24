0faec38539d0ba26fa0afef6a7e152decd251dbd
Lisandro Corbalan <lmcorbalan@gmail.com>
2025-07-24 16:49:58 -0300

    Update HyperlaneOracle for using external contracts instead of removed dependencies

--- src/oracles/hyperlane/HyperlaneOracle.sol
+++ src/oracles/hyperlane/HyperlaneOracle.sol
@@ -1,66 +1,45 @@
 /// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import { GasRouter } from "@hyperlane-xyz/client/GasRouter.sol";
-
 import { IPayloadCreator } from "../../interfaces/IPayloadCreator.sol";
 import { LibAddress } from "../../libs/LibAddress.sol";
 import { MessageEncodingLib } from "../../libs/MessageEncodingLib.sol";
 import { BaseOracle } from "../BaseOracle.sol";
+
+import { MailboxClient } from "./external/hyperlane/MailboxClient.sol";
+import { IMessageRecipient } from "./external/hyperlane/interfaces/IMessageRecipient.sol";
+import { StandardHookMetadata } from "./external/hyperlane/libs/StandardHookMetadata.sol";
 
 /**
  * @notice Hyperlane Oracle.
  * Implements a transparent oracle that allows both sending and receiving messages using Hyperlane protocol along with
  * exposing the hash of received messages.
  */
-contract HyperlaneOracle is GasRouter, BaseOracle {
+contract HyperlaneOracle is BaseOracle, MailboxClient, IMessageRecipient {
     using LibAddress for address;
-
-    /// @dev Reserved storage slots for upgradeability.
-    uint256[47] private __GAP;
 
     error NotAllPayloadsValid();
 
     /**
-     * @notice Initializes the HyperlaneOracle contract with the specified Mailbox address.
-     * @param _mailbox The address of the Hyperlane mailbox contract.
+     * @notice Initializes the HyperlaneOracle contract with the specified Mailbox address, sets the address of the
+     * application's custom interchain security module and the address of the application's custom hook.
+     * @param mailbox The address of the Hyperlane mailbox contract.
+     * @param customHook The address of the custom hook.
+     * @param ism The address of the local ISM contract.
      */
-    constructor(
-        address _mailbox
-    ) GasRouter(_mailbox) { }
+    constructor(address mailbox, address customHook, address ism) MailboxClient(mailbox, customHook, ism) { }
 
     /**
-     * @notice Initializes the Hyperlane router
-     * @param _customHook used by the Router to set the hook to override with
-     * @param _ism The address of the local ISM contract
-     * @param _owner The address with owner privileges
-     */
-    function initialize(address _customHook, address _ism, address _owner) external initializer {
-        _MailboxClient_initialize(_customHook, _ism, _owner);
-    }
-
-    /**
-     * @notice Takes proofs that have been marked as valid by a source and dispatches them to Hyperlane mailbox for
-     * broadcast.
-     * @param originDomain The domain to which the message is sent.
-     * @param source Application that has payloads that are marked as valid.
-     * @param payloads List of payloads to broadcast.
-     */
-    function submit(uint32 originDomain, address source, bytes[] calldata payloads) public payable {
-        if (!IPayloadCreator(source).arePayloadsValid(payloads)) revert NotAllPayloadsValid();
-
-        bytes memory message = MessageEncodingLib.encodeMessage(source.toIdentifier(), payloads);
-        _GasRouter_dispatch(originDomain, msg.value, message, address(hook));
-    }
-
-    /**
-     * @notice Handles incoming messages.
-     * @dev Decodes the message and processes settlement or refund operations accordingly.
+     * @notice Handles incoming Hyperlane messages.
      * @param messageOrigin The domain from which the message originates.
      * @param messageSender The address of the sender on the origin domain. The oracle.
      * @param message The encoded message received via Hyperlane.
      */
-    function _handle(uint32 messageOrigin, bytes32 messageSender, bytes calldata message) internal virtual override {
+    function handle(
+        uint32 messageOrigin,
+        bytes32 messageSender,
+        bytes calldata message
+    ) external payable virtual override onlyMailbox {
         (bytes32 application, bytes32[] memory payloadHashes) = MessageEncodingLib.getHashesOfEncodedPayloads(message);
 
         uint256 numPayloads = payloadHashes.length;
@@ -70,5 +49,65 @@ contract HyperlaneOracle is GasRouter, BaseOracle {
 
             emit OutputProven(messageOrigin, messageSender, application, payloadHash);
         }
+    }
+
+    /**
+     * @notice Takes proofs that have been marked as valid by a source and dispatches them to Hyperlane's Mailbox for
+     * broadcast.
+     * @param destinationDomain The domain to which the message is sent.
+     * @param recipientOracle The address of the oracle on the destination domain.
+     * @param gasLimit Gas limit for the message.
+     * @param customMetadata Additional metadata to include in the standard hook metadata.
+     * @param source Application that has payloads that are marked as valid.
+     * @param payloads List of payloads to broadcast.
+     */
+    function submit(
+        uint32 destinationDomain,
+        address recipientOracle,
+        uint256 gasLimit,
+        bytes calldata customMetadata,
+        address source,
+        bytes[] calldata payloads
+    ) public payable {
+        if (!IPayloadCreator(source).arePayloadsValid(payloads)) revert NotAllPayloadsValid();
+
+        bytes memory message = MessageEncodingLib.encodeMessage(source.toIdentifier(), payloads);
+
+        MAILBOX.dispatch{ value: msg.value }(
+            destinationDomain,
+            recipientOracle.toIdentifier(),
+            message,
+            StandardHookMetadata.formatMetadata(0, gasLimit, msg.sender, customMetadata),
+            hook()
+        );
+    }
+
+    /**
+     * @notice Returns the gas payment required to dispatch a message to the given domain's oracle.
+     * @param destinationDomain The domain to which the message is sent.
+     * @param recipientOracle The address of the oracle on the destination domain.
+     * @param gasLimit Gas limit for the message.
+     * @param customMetadata Additional metadata to include in the standard hook metadata.
+     * @param source Application that has payloads that are marked as valid.
+     * @param payloads List of payloads to broadcast.
+     * @return _gasPayment Payment computed by the registered InterchainGasPaymaster.
+     */
+    function quoteGasPayment(
+        uint32 destinationDomain,
+        address recipientOracle,
+        uint256 gasLimit,
+        bytes calldata customMetadata,
+        address source,
+        bytes[] calldata payloads
+    ) public view returns (uint256) {
+        bytes memory message = MessageEncodingLib.encodeMessage(source.toIdentifier(), payloads);
+
+        return MAILBOX.quoteDispatch(
+            destinationDomain,
+            recipientOracle.toIdentifier(),
+            message,
+            StandardHookMetadata.formatMetadata(0, gasLimit, msg.sender, customMetadata),
+            hook()
+        );
     }
 }
