1945e6be08cbd7c7951a94e9f38bfb995a81730b
Lisandro Corbalan <lmcorbalan@gmail.com>
2025-07-29 12:28:02 -0300

    Update MailboxClient license and sol version

--- src/oracles/hyperlane/external/hyperlane/MailboxClient.sol
+++ src/oracles/hyperlane/external/hyperlane/MailboxClient.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity >=0.6.11;
+/// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
 import { IInterchainSecurityModule } from "./interfaces/IInterchainSecurityModule.sol";
 import { IMailbox } from "./interfaces/IMailbox.sol";
