dd6c392de8c914600de1e471fd2a59a00833d918
Alexander <git@lindgren.xyz>
2025-07-30 15:49:55 +0200

    Significantly optimise the order identifier computation for multichain escrows

--- src/input/types/MultichainOrderComponentType.sol
+++ src/input/types/MultichainOrderComponentType.sol
@@ -54,6 +54,7 @@ library MultichainOrderComponentType {
      * - c: [c, [3, 1], [3,2]] => hc = keccak256(abi.encodePacked("c", c))
      * And wants to compute the hash: h = keccak256(abi.encodePacked(a, b, c)))
      * Given 1, b, and [ha, hc] the function will compute h.
+     * TODO: test that this works as expected against non-solidity version.
      */
     function constructInputHash(
         uint256 inputsChainId,
@@ -63,24 +64,59 @@ library MultichainOrderComponentType {
     ) internal pure returns (bytes32) {
         bytes32 inputHash = hashInputs(inputsChainId, inputs);
         uint256 numSegments = additionalChains.length + 1;
-        // TODO: chainIndex in [0, numSegments]
-        // TODO: use assembly for insert.
-        bytes32[] memory claimStructure = new bytes32[](numSegments);
+        // TODO: check that chainIndex is in range [0, numSegments]
+        bytes memory claimStructure = new bytes(32 * numSegments);
+        uint256 p;
+        assembly ("memory-safe") {
+            p := add(claimStructure, 0x20)
+        }
         for (uint256 i = 0; i < numSegments; ++i) {
             uint256 additionalChainsIndex;
             assembly ("memory-safe") {
                 // If we have already inserted "inputs" we need to remove 1 from the index.
                 additionalChainsIndex := sub(i, gt(i, chainIndex))
             }
-            claimStructure[i] = chainIndex == i ? inputHash : additionalChains[additionalChainsIndex];
+            bytes32 inputHashElement = chainIndex == i ? inputHash : additionalChains[additionalChainsIndex];
+            assembly ("memory-safe") {
+                mstore(add(p, mul(i, 0x20)), inputHashElement)
+            }
         }
         // Length is implied by size.
-        return keccak256(abi.encodePacked(claimStructure));
+        return keccak256(claimStructure);
     }
 
-    function hashInputs(uint256 chainId, uint256[2][] calldata inputs) internal pure returns (bytes32) {
-        // TODO: If we use this straight, change to assembly calldata copy.
-        // TODO: Alternative is to use abi.encodePacked. Length is implicit though size.
-        return keccak256(abi.encodePacked(chainId, inputs));
+    /**
+     * @notice Internal pure function for deriving the hash of ids and amounts with a provided chainId salt.
+     * TODO: test that the below statement is true.
+     * This function returns keccak256(abi.encodePacked(chainId, idsAndAmounts))
+     * @param chainId Chain identifier used to salt the input hash.
+     * @param idsAndAmounts An array of ids and amounts.
+     * @return inputHash The hash of the ids and amounts salted with chainId.
+     * @dev This function expects that the calldata of idsAndAmounts will have bounds
+     * checked elsewhere; using it without this check occurring elsewhere can result in
+     * erroneous hash values.
+     */
+    function hashInputs(uint256 chainId, uint256[2][] calldata idsAndAmounts)
+        internal
+        pure
+        returns (bytes32 inputHash)
+    {
+        assembly ("memory-safe") {
+            // Retrieve the free memory pointer; memory will be left dirtied.
+            let ptr := mload(0x40)
+
+            // Get the total length of the calldata slice.
+            // Each element of the array consists of 2 words.
+            let len := mul(idsAndAmounts.length, 0x40)
+
+            // Store the chainId at the pointer.
+            mstore(ptr, chainId)
+
+            // Copy calldata into memory after the chainId.
+            calldatacopy(add(0x20, ptr), idsAndAmounts.offset, len)
+
+            // Compute the hash of the calldata that has been copied into memory.
+            inputHash := keccak256(ptr, add(len, 0x20))
+        }
     }
 }
